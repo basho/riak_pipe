8289f3cc2f6217875cc9045fbeda8a678548f9b6
Bryan Fink <bryan@basho.com>
2013-10-01 15:18:21 -0400

    simplify riak_pipe_builder_sup:builder_pids return value
    This removes the use of types not exported from 'supervisor' while also making
    the return value match the name of the function.

--- src/riak_pipe_cinfo.erl
+++ src/riak_pipe_cinfo.erl
@@ -47,7 +47,7 @@ cluster_info_generator_funs() ->
 pipelines(C) ->
     Pipes = riak_pipe_builder_sup:builder_pids(),
     cluster_info:format(C, "Pipelines active: ~b~n", [length(Pipes)]),
-    [ pipeline(C, Pipe) || {_Id, Pipe, _Type, _Mods} <- Pipes],
+    _ = [ pipeline(C, Pipe) || Pipe <- Pipes],
     ok.
 
 %% @doc Print information about the given pipeline.
